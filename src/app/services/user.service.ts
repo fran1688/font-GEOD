33ba885a7007287b9358fd0407d410afb429e51d
Francisco Castillo <ext_fjocastillo@falabella.cl>
2020-09-09 21:15:21 -0300

    de agregan nuevas imagenes

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
+
+import {environment} from '../../environments/environment';
+
+import {RegisterForm} from '../interfaces/register-form.interface';
+import {LoginForm} from '../interfaces/login-form.interface';
+import {Observable, of} from 'rxjs';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor( private  http: HttpClient ) { }
+
+  validToken(): Observable<boolean> {
+    const token = localStorage.getItem('token') || '';
+
+    return this.http.get(`${ base_url }/login/renew`, {
+      headers: {
+        'token': token
+      }
+    }).pipe(
+      tap( (resp: any) => {
+        localStorage.setItem('token', resp.token);
+      }),
+      map( resp => true),
+      catchError( error => of(false) )
+    );
+  }
+
+  createUser( formData: RegisterForm) {
+    return this.http.post(`${ base_url }/users`, formData)
+      .pipe(
+        tap( (resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
+  }
+
+  login( formData: LoginForm) {
+    return this.http.post(`${ base_url }/login`, formData)
+      .pipe(
+        tap( (resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
+  }
+
+  loginGoogle( token ) {
+    return this.http.post(`${ base_url }/login/google`, {token})
+      .pipe(
+        tap( (resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
+  }
+
+}
